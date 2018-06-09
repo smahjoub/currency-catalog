a300b2a7da5793e9183c854f1c20e7dbb4b4a909
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2018-06-09 16:54:58 +0200

    add currencies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { CurrenciesComponent } from './components/currencies/currencies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CurrenciesComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CurrenciesComponent]
 })
 export class AppModule { }
